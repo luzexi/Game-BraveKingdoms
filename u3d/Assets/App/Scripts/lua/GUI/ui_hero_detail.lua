56e89a3951daf15d1976a643b7b5e0652a8843df
luzexi <jesse_luzexi@163.com>
2015-02-15 02:42:24 +0800

    add ui prefab

--- u3d/Assets/App/Scripts/lua/GUI/ui_hero_detail.lua
+++ u3d/Assets/App/Scripts/lua/GUI/ui_hero_detail.lua
@@ -3,22 +3,31 @@ local GameObject = UnityEngine.GameObject
 local Resources = UnityEngine.Resources
 local Vector3 = UnityEngine.Vector3
 
-local ui_name = "ui_hero_detail"
+local ui_hero_detail = nil
 
 local function create( func )
-    ui_hero_detail = GameObject.Instantiate( Resources.Load("GUI/ui_hero_detail") )
-    ui_hero_detail.name = ui_name
-    ui_hero_detail.transform:SetParent(UI_Root)
-    ui_hero_detail.transform.localPosition = Vector3.zero
-    ui_hero_detail.transform.localScale = Vector3.one
+    local ui_name = "ui_hero_detail"
 
-    local collider = ui_hero_detail.transform:Find("collider")
-    local ev = UI_Event.Get(collider)
-    ev.onClick = {"+=",function(eventData , go , args)
-        GameObject.Destroy(ui_hero_detail)
-        func()
+    local function createObj()
+        ui_hero_detail = GameObject.Instantiate( Resources.Load("GUI/ui_hero_detail") )
+        ui_hero_detail.name = ui_name
+        ui_hero_detail.transform:SetParent(UI_Root)
+        ui_hero_detail.transform.localPosition = Vector3.zero
+        ui_hero_detail.transform.localScale = Vector3.one
     end
-    }
+
+    local function regEvent()
+        local collider = ui_hero_detail.transform:Find("collider")
+        local ev = UI_Event.Get(collider)
+        ev.onClick = {"+=",function(eventData , go , args)
+            GameObject.Destroy(ui_hero_detail)
+            func()
+        end
+        }
+    end
+
+    createObj()
+    regEvent()
 end
 
 
--- u3d/Assets/App/Scripts/lua/GUI/ui_main.lua
+++ u3d/Assets/App/Scripts/lua/GUI/ui_main.lua
@@ -7,33 +7,40 @@ local Resources = UnityEngine.Resources
 local Vector3 = UnityEngine.Vector3
 
 
-local ui_name = "ui_main"
+
 
 -- create
 local function create()
-    ui_main_obj = GameObject.Instantiate( Resources.Load("GUI/ui_main") )
-    ui_main_obj.name = ui_name
-    ui_main_obj.transform:SetParent(UI_Root)
-    ui_main_obj.transform.localScale = Vector3.one
-    ui_main_obj.transform.localPosition = Vector3.zero
+    local ui_main_obj = nil
+    local ui_name = "ui_main"
 
-    local btn1 = ui_main_obj.transform:Find("icon/btn1")
-    local ev = UI_Event.Get(btn1)
-    ev.onClick = {"+=", function( eventData , go , args )
-        print("destory ui main obj ")
-        if ui_main_obj == nil then
-            print(" ui main is nil")
-        end
-        GameObject.Destroy(ui_main_obj)
-
-        ui_hero_detail.create( function()
-            ui_system_bottom.show()
-            create()
-        end
-        )
-        ui_system_bottom.hiden()
+    local function createObj()
+        ui_main_obj = GameObject.Instantiate( Resources.Load("GUI/ui_main") )
+        ui_main_obj.name = ui_name
+        ui_main_obj.transform:SetParent(UI_Root)
+        ui_main_obj.transform.localScale = Vector3.one
+        ui_main_obj.transform.localPosition = Vector3.zero
     end
-    }
+
+    local function regEvent()
+        local btn1 = ui_main_obj.transform:Find("icon/btn1")
+        local ev = UI_Event.Get(btn1)
+        ev.onClick = {"+=", function( eventData , go , args )
+            GameObject.Destroy(ui_main_obj)
+
+            ui_hero_detail.create( function()
+                ui_system_bottom.show()
+                createObj()
+                regEvent()
+            end
+            )
+            ui_system_bottom.hiden()
+        end
+        }
+    end
+
+    createObj()
+    regEvent()
 end
 
 
